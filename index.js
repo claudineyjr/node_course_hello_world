c772498238671b9bf144169f206b86f86d6d95b0
Claudiney Junior <claudiney.alves@fatec.sp.gov.br>
2018-07-25 21:37:53 -0300

    Exemplos de sintaxe de funções

--- index.js
+++ index.js
@@ -2,3 +2,21 @@ const name = 'Claudiney Junior' // Preencha com seu nome
 
 console.log(`Olá ${name}`);
 
+function sub(x, y) {
+  return x - y;
+}
+
+const add = (x, y) => {
+  return x + y;
+};
+
+
+const multiplicate = (x, y) => {
+  return x * y;
+};
+
+const calculate = (operation, x, y) => {
+  return operation(x, y);
+};
+
+console.log(calculate(sub, x, y));
